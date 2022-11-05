5b79927daa6062720f394fac688a95b445eba893
Diego Ramirez <da.ramirez55@uniandes.edu.co>
2022-11-06 00:57:51 +0100

    Agregado total de tipos de café

--- src/app/cafes/cafe-list/cafe-list.component.ts
+++ src/app/cafes/cafe-list/cafe-list.component.ts
@@ -7,17 +7,39 @@ import { CafeService } from '../cafe.service';
   templateUrl: './cafe-list.component.html',
   styleUrls: ['./cafe-list.component.css']
 })
+
 export class CafeListComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
+  totalBlend: number = 0;
+  totalOrigen: number = 0;
 
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void {
     this.cafeService.getCafes().subscribe((cafes) => {
       this.cafes = cafes;
+      this.calculateBlend(cafes);
+      this.calculateOrigen(cafes);
     });
   }
+
+  calculateBlend(listadoCafe : Array<Cafe>): void {
+    listadoCafe.forEach( x => {
+      if (x.tipo == "Blend"){
+        this.totalBlend += 1;
+      }
+    })
+  }
+
+  calculateOrigen(listadoCafe : Array<Cafe>): void {
+    listadoCafe.forEach( x => {
+      if (x.tipo == "Café de Origen"){
+        this.totalOrigen += 1;
+      }
+    })
+  }
+
 
   ngOnInit() {
     this.getCafes();
