34c9a2938311a6542d99fc308d68b2a292ced05b
Diego Ramirez <da.ramirez55@uniandes.edu.co>
2022-11-05 22:22:14 +0100

    Primer commit: modulo, clase cafe y servicio que consume el listado de cafes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CafesModule } from './cafes/cafes.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CafesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
